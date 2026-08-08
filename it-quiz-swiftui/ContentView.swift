9156990e82f04253e3d93627495429d81675922d
Ivan Krugov <sevagote@gmail.com>
2026-08-08 09:22:20 +0300

    Initial Commit

---
+++ it-quiz-swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  it-quiz-swiftui
+//
+//  Created by Ivan Krugov on 08.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ it-quiz-swiftui/it_quiz_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  it_quiz_swiftuiApp.swift
+//  it-quiz-swiftui
+//
+//  Created by Ivan Krugov on 08.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct it_quiz_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
